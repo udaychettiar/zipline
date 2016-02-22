0929a81c4b439c275258055ee2afbd6787d97191
Maya Tydykov <mtydykov@quantopian.com>
2016-02-22 10:46:41 -0500

    MAINT: remove obsolete and alphabetize.

--- zipline/pipeline/loaders/blaze/__init__.py
+++ zipline/pipeline/loaders/blaze/__init__.py
@@ -4,21 +4,13 @@ from .buyback_auth import (
     BlazeShareBuybackAuthorizationsLoader
 )
 from .core import (
-    AD_FIELD_NAME,
     BlazeLoader,
     NoDeltasWarning,
-    SID_FIELD_NAME,
-    TS_FIELD_NAME,
     from_blaze,
     global_loader,
 )
-from .buyback_auth import (
-    BUYBACK_ANNOUNCEMENT_FIELD_NAME,
-    SHARE_COUNT_FIELD_NAME,
-    CASH_FIELD_NAME
-)
+
 from .earnings import (
-    ANNOUNCEMENT_FIELD_NAME,
     BlazeEarningsCalendarLoader,
 )
 
@@ -27,7 +19,7 @@ __all__ = (
     'BlazeEarningsCalendarLoader',
     'BlazeLoader',
     'BlazeShareBuybackAuthorizationsLoader',
-    'NoDeltasWarning',
     'from_blaze',
     'global_loader',
+    'NoDeltasWarning',
 )
