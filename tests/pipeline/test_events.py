d53eea59a88240ca05faf08f19a0069284219b38
Maya Tydykov <mtydykov@quantopian.com>
2016-02-22 10:46:01 -0500

    TST: clean up/extract constants in tests.

--- tests/pipeline/test_events.py
+++ tests/pipeline/test_events.py
@@ -5,9 +5,10 @@ from nose_parameterized import parameterized
 
 import blaze as bz
 import pandas as pd
-from pandas.util.testing import assert_series_equal, TestCase, assertRaises
+from pandas.util.testing import assert_series_equal, TestCase
 
 from zipline.pipeline.common import (
+    ANNOUNCEMENT_FIELD_NAME,
     SID_FIELD_NAME,
     TS_FIELD_NAME
 )
@@ -25,12 +26,6 @@ from zipline.utils.memoize import lazyval
 from zipline.utils.numpy_utils import datetime64ns_dtype
 
 ABSTRACT_METHODS_ERROR = 'abstract methods concrete_loader'
-
-DAYS_SINCE_PREV = 'days_since_prev'
-
-PREVIOUS_ANNOUNCEMENT = 'previous_announcement'
-
-ANNOUNCEMENT_FIELD_NAME = 'announcement_date'
 
 
 class EventDataSet(DataSet):
@@ -88,28 +83,32 @@ class EventDataSetLoaderNoExpectedCols(EventsLoader):
 dtx = pd.date_range('2014-01-01', '2014-01-10')
 
 
-def assert_loader_error(events_by_sid, error, msg, infer_timestamps=True):
-    with assertRaises(error) as context:
+def assert_loader_error(events_by_sid, error, msg, infer_timestamps):
+    with TestCase.assertRaises(error) as context:
         EventDataSetLoader(
             dtx, events_by_sid, infer_timestamps=infer_timestamps,
         )
-        assert msg in context.exception
+        TestCase.assertTrue(msg in context.exception)
 
 
 class EventLoaderTestCase(TestCase):
 
     def test_no_expected_cols_defined(self):
         events_by_sid = {0: pd.DataFrame({ANNOUNCEMENT_FIELD_NAME: dtx})}
-        assert_loader_error(events_by_sid, TypeError, ABSTRACT_METHODS_ERROR)
+        assert_loader_error(events_by_sid, TypeError, ABSTRACT_METHODS_ERROR,
+                            True)
 
     def test_wrong_cols(self):
         wrong_col_name = 'some_other_col'
         # Test wrong cols (cols != expected)
         events_by_sid = {0: pd.DataFrame({wrong_col_name: dtx})}
         assert_loader_error(
-            events_by_sid, ValueError, WRONG_COLS_ERROR % (
-                EventDataSetLoader.expected_cols, 0, wrong_col_name
-            )
+            events_by_sid, ValueError, WRONG_COLS_ERROR.format(
+                expected_columns=EventDataSetLoader.expected_cols,
+                sid=0,
+                resulting_columns=wrong_col_name,
+            ),
+            True
         )
 
     @parameterized.expand([
@@ -137,29 +136,34 @@ class EventLoaderTestCase(TestCase):
         )
 
         if infer_timestamps:
-            expected = pd.Series(index=[dtx[0]] * 10, data=dtx, )
+            expected = pd.Series(index=[dtx[0]] * 10, data=dtx,
+                                 name=ANNOUNCEMENT_FIELD_NAME)
         else:
-            expected = pd.Series(index=dtx, data=dtx,)
+            expected = pd.Series(index=dtx, data=dtx,
+                                 name=ANNOUNCEMENT_FIELD_NAME)
+            expected.index.name = TS_FIELD_NAME
         # Check that index by first given date has been added
         assert_series_equal(
             loader.events_by_sid[0][ANNOUNCEMENT_FIELD_NAME],
             expected,
-            check_names=False
         )
 
     @parameterized.expand([
         # DataFrame without timestamp column and infer_timestamps = True
         [pd.DataFrame({ANNOUNCEMENT_FIELD_NAME: dtx}), False,
-         DF_NO_TS_NOT_INFER_TS_ERROR % (TS_FIELD_NAME, 0)],
+         DF_NO_TS_NOT_INFER_TS_ERROR.format(
+             timestamp_column_name=TS_FIELD_NAME,
+             sid=0
+         )],
         # DatetimeIndex with infer_timestamps = False
         [pd.DatetimeIndex(dtx, name=ANNOUNCEMENT_FIELD_NAME), False,
-         DTINDEX_NOT_INFER_TS_ERROR % 0],
+         DTINDEX_NOT_INFER_TS_ERROR.format(sid=0)],
         # Series with DatetimeIndex as index and infer_timestamps = False
         [pd.Series(dtx, name=ANNOUNCEMENT_FIELD_NAME), False,
-         SERIES_NO_DTINDEX_ERROR % 0],
+         SERIES_NO_DTINDEX_ERROR.format(sid=0)],
         # Some other data structure that is not expected
-        [dtx, False, BAD_DATA_FORMAT_ERROR % 0],
-        [dtx, True, BAD_DATA_FORMAT_ERROR % 0]
+        [dtx, False, BAD_DATA_FORMAT_ERROR.format(sid=0)],
+        [dtx, True, BAD_DATA_FORMAT_ERROR.format(sid=0)]
     ])
     def test_bad_conversion_to_df(self, df, infer_timestamps, msg):
         events_by_sid = {0: df}
@@ -182,7 +186,7 @@ class BlazeEventDataSetLoaderNoConcreteLoader(BlazeEventsLoader):
 class BlazeEventLoaderTestCase(TestCase):
     # Blaze loader: need to test failure if no concrete loader
     def test_no_concrete_loader_defined(self):
-        with assertRaises(TypeError) as context:
+        with TestCase.assertRaises(TypeError) as context:
             BlazeEventDataSetLoaderNoConcreteLoader(
                 bz.Data(
                     pd.DataFrame({ANNOUNCEMENT_FIELD_NAME: dtx,
@@ -190,4 +194,4 @@ class BlazeEventLoaderTestCase(TestCase):
                                   })
                 )
             )
-            assert ABSTRACT_METHODS_ERROR in context.exception
+            TestCase.assertTrue(ABSTRACT_METHODS_ERROR in context.exception)
